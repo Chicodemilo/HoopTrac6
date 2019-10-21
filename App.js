cd193816eee306b3cb317b5deaf9b0d8a693903d
Chicodemilo <mileschick@gmail.com>
2019-10-21 05:49:55 -0500

    Fixes from RN6 migration

--- App.js
+++ App.js
@@ -1,250 +1,296 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
 import {
-    StyleSheet,
-    Text,
-    View,
-    Button,
-    ImageBackground,
-    Keyboard,
-    Platform
-} from "react-native";
-import PlayerInput from "./src/components/PlayerInput/PlayerInput";
-import PlayerList from "./src/components/PlayerList/PlayerList";
-import StartButton from "./src/components/Game/StartButton";
-import courtImage from "./src/assets/court3.jpg";
-import StatsContainer from "./src/components/Stats/StatsContainer";
-import GameContainer from "./src/components/Game/GameContainer";
-import StatsService from "./src/services/StatsService";
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ImageBackground,
+  Keyboard,
+  Platform,
+} from 'react-native';
+import {format, getTime} from 'date-fns';
+import PlayerInput from './src/components/PlayerInput/PlayerInput';
+import PlayerList from './src/components/PlayerList/PlayerList';
+import StartButton from './src/components/Game/StartButton';
+import courtImage from './src/assets/court3.jpg';
+import StatsContainer from './src/components/Stats/StatsContainer';
+import GameContainer from './src/components/Game/GameContainer';
+import StatsService from './src/services/StatsService';
 
 export default class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            inputMessage: "Enter Player Name",
-            playersObj: {},
-            initialPlayerObj: {},
-            playerLimit: 3,
-            disableButton: true,
-            gameInProgress: false,
-            showFinalStats: false,
-            gameTime: 0,
-            endGameStats: "",
-            firstActivePlayerName: "",
-            firstActivePlayerKey: null
-        };
+  constructor() {
+    super();
+    this.state = {
+      inputMessage: 'Enter Player Name',
+      playersObj: {},
+      initialPlayerObj: {},
+      playerLimit: 3,
+      disableButton: true,
+      gameInProgress: false,
+      showFinalStats: false,
+      gameTime: 0,
+      endGameStats: '',
+      firstActivePlayerName: '',
+      firstActivePlayerKey: null,
+      gameRestart: false,
+    };
+  }
+
+  playerAddedHandler = playerName => {
+    const activatePlayer =
+      Object.keys(this.state.playersObj).length > 0 ? false : true;
+
+    const playerKey = new Date().getTime();
+
+    if (activatePlayer == true) {
+      this.setState({
+        firstActivePlayerKey: playerKey,
+        firstActivePlayerName: playerName,
+      });
     }
 
-    playerAddedHandler = playerName => {
-        const activatePlayer = Object.keys(this.state.playersObj).length > 0 ? false : true;
+    let thisPlayer = {
+      key: playerKey,
+      name: playerName,
+      active: activatePlayer,
+      clockedIn: false,
+      timePlayedSec: 0,
+      timePlayedMin: '0:00',
+      percentOfGamePlayed: 0,
+      points: 0,
+      shotAttempts: 0,
+      shotsMade: 0,
+      shootingPercentage: 0,
+      twoPointAttempts: 0,
+      twoPointMade: 0,
+      threePointAttempts: 0,
+      threePointMade: 0,
+      threePointPercentage: 0,
+      rebounds: 0,
+      offRebounds: 0,
+      defRebounds: 0,
+      assists: 0,
+      steals: 0,
+      blocks: 0,
+      turnOvers: 0,
+      foulsCommitted: 0,
+      technicals: 0,
+      freeThrowAttempts: 0,
+      freeThrowMade: 0,
+      freeThrowPercentage: 0,
+      pointsPerMin: 0,
+      shotsPerMin: 0,
+      reboundsPerMin: 0,
+      assistsPerMin: 0,
+      blocksPerMin: 0,
+      turnOversPerMin: 0,
+      foulsPerMin: 0,
+      efficiencyRating: 0,
+      assistToTurnOver: 0,
+    };
+    Keyboard.dismiss();
+    this.setState(prevState => {
+      return {
+        playerObj: (prevState.playersObj[thisPlayer.key] = thisPlayer),
+        disableButton: false,
+      };
+    });
+  };
 
-        const playerKey = new Date().getTime();
+  toggleCheckIn = key => {
+    this.setState(prevState => {
+      prevState.playersObj[key].clockedIn = !prevState.playersObj[key]
+        .clockedIn;
+      return {
+        players: prevState.playersObj,
+      };
+    });
+  };
 
-        if (activatePlayer == true) {
-            this.setState({
-                firstActivePlayerKey: playerKey,
-                firstActivePlayerName: playerName
-            });
-        }
+  playerDeletedHandler = key => {
+    let buttonOff =
+      Object.keys(this.state.playersObj).length > 1 ? false : true;
+    this.setState(prevState => {
+      delete prevState.playersObj[key];
+      return {
+        players: prevState.playersObj,
+        inputMessage: 'Enter Player Name',
+        disableButton: buttonOff,
+      };
+    });
+  };
 
-        thisPlayer = {
-            key: playerKey,
-            name: playerName,
-            active: activatePlayer,
-            clockedIn: false,
-            timePlayedSec: 0,
-            timePlayedMin: "0:00",
-            percentOfGamePlayed: 0,
-            points: 0,
-            shotAttempts: 0,
-            shotsMade: 0,
-            shootingPercentage: 0,
-            twoPointAttempts: 0,
-            twoPointMade: 0,
-            threePointAttempts: 0,
-            threePointMade: 0,
-            threePointPercentage: 0,
-            rebounds: 0,
-            offRebounds: 0,
-            defRebounds: 0,
-            assists: 0,
-            steals: 0,
-            blocks: 0,
-            turnOvers: 0,
-            foulsCommitted: 0,
-            technicals: 0,
-            freeThrowAttempts: 0,
-            freeThrowMade: 0,
-            freeThrowPercentage: 0,
-            pointsPerMin: 0,
-            shotsPerMin: 0,
-            reboundsPerMin: 0,
-            assistsPerMin: 0,
-            blocksPerMin: 0,
-            turnOversPerMin: 0,
-            foulsPerMin: 0,
-            efficiencyRating: 0,
-            assistToTurnOver: 0
-        };
-        Keyboard.dismiss();
-        this.setState(prevState => {
-            return {
-                playerObj: (prevState.playersObj[thisPlayer.key] = thisPlayer),
-                disableButton: false
-            };
-        });
+  startTheGameHandler = index => {
+    // console.log(this.state.endGameStats);
+    this.setState(prevState => {
+      return {
+        endGameStats: {},
+        gameInProgress: true,
+      };
+    });
+  };
+
+  gameEndHandler = (endGameTime, endGameStats, endGameSeconds) => {
+    Object.keys(endGameStats).map(key => {
+      endGameStats[key].clockedIn = false;
+    });
+
+    const calculatedStats = StatsService.calculateFinalStats(
+      endGameStats,
+      endGameSeconds,
+    );
+
+    const endGame = {
+      id: this.makeGameId(),
+      date: format(new Date(), 'yyyy-MM-dd'),
+      name: 'BBall Game: ' + format(new Date(), 'M/d/yyyy'),
+      players: endGameStats,
+      gameTime: endGameTime,
     };
 
-    toggleCheckIn = key => {
-        this.setState(prevState => {
-            prevState.playersObj[key].clockedIn = !prevState.playersObj[key].clockedIn;
-            return {
-                players: prevState.playersObj
-            };
-        });
-    };
+    this.setState(prevState => {
+      return {
+        disableButton: true,
+        playersObj: {},
+        gameInProgress: false,
+        endGameStats: endGame,
+        gameTime: endGameTime,
+      };
+    });
+  };
 
-    playerDeletedHandler = key => {
-        buttonOff = Object.keys(this.state.playersObj).length > 1 ? false : true;
-        this.setState(prevState => {
-            delete prevState.playersObj[key];
-            return {
-                players: prevState.playersObj,
-                inputMessage: "Enter Player Name",
-                disableButton: buttonOff
-            };
-        });
-    };
+  makeGameId = () => {
+    let id = Math.floor(getTime(new Date()) / 1000);
+    id = id + Math.floor(Math.random() * 10000);
+    return id;
+  };
 
-    startTheGameHandler = index => {
-        // console.log(this.state.endGameStats);
-        this.setState(prevState => {
-            return {
-                endGameStats: {},
-                gameInProgress: true
-            };
-        });
-    };
+  showFinalStats = () => {
+    this.setState({
+      showFinalStats: true,
+    });
+  };
 
-    gameEndHandler = (endGameTime, endGameStats, endGameSeconds) => {
-        Object.keys(endGameStats).map(key => {
-            endGameStats[key].clockedIn = false;
-        });
-        const calculatedStats = StatsService.calculateFinalStats(
-            endGameStats,
-            endGameSeconds
-        );
-        this.setState(prevState => {
-            return {
-                disableButton: true,
-                playersObj: {},
-                gameInProgress: false,
-                endGameStats: endGameStats,
-                gameTime: endGameTime
-            };
-        });
-    };
+  hideFinalStats = () => {
+    this.setState({
+      showFinalStats: false,
+    });
+  };
 
-    showFinalStats = () => {
-        this.setState({
-            showFinalStats: true
-        });
-    };
+  render() {
+    let finalStatsButton =
+      this.state.endGameStats != '' ? (
+        <View style={styles.statsRestartButtons}>
+          <Text style={styles.gameTitle}>{this.state.endGameStats.name}</Text>
+          <Button
+            style={styles.baseButton}
+            title="Show Stats"
+            color="#cc5500"
+            onPress={() => {
+              this.showFinalStats();
+            }}
+          />
+          <Button
+            style={styles.baseButton}
+            title="Resume Game"
+            color="#448ccf"
+            onPress={() => {
+              this.showFinalStats();
+            }}
+          />
+        </View>
+      ) : null;
 
-    hideFinalStats = () => {
-        this.setState({
-            showFinalStats: false
-        });
-    };
-
-    render() {
-        let finalStatsButton =
-            this.state.endGameStats != "" ? (
-                <View>
-                    <Button
-                        style={styles.baseButton}
-                        title="Show Game Stats"
-                        color="blue"
-                        onPress={() => {
-                            this.showFinalStats();
-                        }}
-                    />
-                </View>
-            ) : null;
-
-        let endGameView = null;
-        if (this.state.showFinalStats == true) {
-            endGameView = (
-                <StatsContainer
-                    showFinalStats={this.state.showFinalStats}
-                    hideFinalStats={this.hideFinalStats}
-                    finalStats={this.state.endGameStats}
-                    gameTime={this.state.gameTime}
-                />
-            );
-        }
-
-        return (
-            <ImageBackground source={courtImage} style={{ width: "100%", height: "100%" }}>
-                <View style={styles.container}>
-                    {Object.keys(this.state.playersObj).length > 0 ? (
-                        <GameContainer
-                            gamePlayers={this.state.playersObj}
-                            initialPlayers={this.state.initialPlayerObj}
-                            gameInProgress={this.state.gameInProgress}
-                            gameEnd={this.gameEndHandler}
-                            firstActivePlayerKey={this.state.firstActivePlayerKey}
-                            firstActivePlayerName={this.state.firstActivePlayerName}
-                        />
-                    ) : null}
-
-                    {endGameView}
-
-                    <StartButton
-                        startTheGame={this.startTheGameHandler}
-                        allowGame={this.state.disableButton}
-                    />
-                    <Text style={styles.welcome}>HoopTrack</Text>
-                    <PlayerInput
-                        onPlayerAdded={this.playerAddedHandler}
-                        maxPlayers={this.state.playerLimit}
-                        playerCount={Object.keys(this.state.playersObj).length}
-                    />
-                    <PlayerList
-                        players={this.state.players}
-                        playerObj={this.state.playersObj}
-                        onItemDeleted={this.playerDeletedHandler}
-                        onPlayerCheckIn={this.toggleCheckIn}
-                    />
-                    {finalStatsButton}
-                </View>
-            </ImageBackground>
-        );
+    let endGameView = null;
+    if (this.state.showFinalStats == true) {
+      endGameView = (
+        <StatsContainer
+          showFinalStats={this.state.showFinalStats}
+          hideFinalStats={this.hideFinalStats}
+          finalStats={this.state.endGameStats}
+          gameTime={this.state.gameTime}
+        />
+      );
     }
+
+    return (
+      <ImageBackground
+        source={courtImage}
+        style={{width: '100%', height: '100%'}}>
+        <View style={styles.container}>
+          {Object.keys(this.state.playersObj).length > 0 ? (
+            <GameContainer
+              gamePlayers={this.state.playersObj}
+              initialPlayers={this.state.initialPlayerObj}
+              gameInProgress={this.state.gameInProgress}
+              gameEnd={this.gameEndHandler}
+              firstActivePlayerKey={this.state.firstActivePlayerKey}
+              firstActivePlayerName={this.state.firstActivePlayerName}
+            />
+          ) : null}
+
+          {endGameView}
+
+          <StartButton
+            startTheGame={this.startTheGameHandler}
+            allowGame={this.state.disableButton}
+          />
+          <Text style={styles.welcome}>HoopTrac</Text>
+          <PlayerInput
+            onPlayerAdded={this.playerAddedHandler}
+            maxPlayers={this.state.playerLimit}
+            playerCount={Object.keys(this.state.playersObj).length}
+          />
+          <PlayerList
+            players={this.state.players}
+            playerObj={this.state.playersObj}
+            onItemDeleted={this.playerDeletedHandler}
+            onPlayerCheckIn={this.toggleCheckIn}
+          />
+          {finalStatsButton}
+        </View>
+      </ImageBackground>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: "column",
-        justifyContent: "flex-start",
-        alignItems: "center",
-        backgroundColor: "rgba(255, 255, 255, 0.6)",
-        marginTop: 0,
-        marginBottom: 0,
-        paddingTop: Platform.OS === "ios" ? 50 : 15,
-        paddingBottom: 30
-    },
-    welcome: {
-        fontSize: 60,
-        color: "#cc5500",
-        fontWeight: "bold",
-        textAlign: "center",
-        margin: 10
-    },
-    courtPic: {
-        height: 500,
-        width: 300,
-        zIndex: -5,
-        transform: [{ rotate: "90deg" }]
-    }
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.6)',
+    marginTop: 0,
+    marginBottom: 0,
+    paddingTop: Platform.OS === 'ios' ? 50 : 15,
+    paddingBottom: 30,
+  },
+  welcome: {
+    fontSize: 60,
+    color: '#cc5500',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 10,
+  },
+  courtPic: {
+    height: 500,
+    width: 300,
+    zIndex: -5,
+    transform: [{rotate: '90deg'}],
+  },
+  statsRestartButtons: {
+    padding: 7,
+    textAlign: 'center',
+    width: '100%',
+    backgroundColor: 'rgba(255,255,255,0.3)',
+    borderBottomColor: '#521800',
+    borderTopColor: '#521800',
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
+  },
+  gameTitle: {
+    width: '100%',
+    textAlign: 'center',
+  },
 });
--- src/components/Game/StartButton.js
+++ src/components/Game/StartButton.js
@@ -1,60 +1,60 @@
-import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Button } from "react-native";
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, Button} from 'react-native';
 
 const startButton = props => (
-    <View style={styles.buttonContainer}>
-        <TouchableOpacity
-            onPress={props.startTheGame}
-            disabled={props.allowGame}
-            style={this.buttonStyle(props.allowGame)}
-        >
-            <View style={styles.button}>
-                <Text style={this.buttonTextStyle(props.allowGame)}>Start Game</Text>
-            </View>
-        </TouchableOpacity>
-    </View>
+  <View style={styles.buttonContainer}>
+    <TouchableOpacity
+      onPress={props.startTheGame}
+      disabled={props.allowGame}
+      style={this.buttonStyle(props.allowGame)}>
+      <View style={styles.button}>
+        <Text style={this.buttonTextStyle(props.allowGame)}>Start Game</Text>
+      </View>
+    </TouchableOpacity>
+  </View>
 );
 
 buttonStyle = allow => {
-    thisStyle = allow ? styles.disableButton : styles.startButton;
-    return thisStyle;
+  thisStyle = allow ? styles.disableButton : styles.startButton;
+  return thisStyle;
 };
 
 buttonTextStyle = allow => {
-    thisStyle = allow ? styles.disableButtonText : styles.startButtonText;
-    return thisStyle;
+  thisStyle = allow ? styles.disableButtonText : styles.startButtonText;
+  return thisStyle;
 };
 
 const styles = StyleSheet.create({
-    buttonContainer: {
-        // flex: 1,
-        width: "100%",
-        borderBottomColor: "grey",
-        borderBottomWidth: 1,
-        flexDirection: "column",
-        justifyContent: "space-between",
-        alignItems: "center",
-        padding: 10
-    },
-    startButton: {
-        width: "100%",
-        padding: 12,
-        alignSelf: "stretch",
-        backgroundColor: "#cc5500",
-        alignItems: "center"
-    },
-    disableButton: {
-        width: "100%",
-        alignItems: "center",
-        backgroundColor: "#rgba(0, 0, 0, 0.3)",
-        padding: 12
-    },
-    startButtonText: {
-        color: "white"
-    },
-    disableButtonText: {
-        color: "#787878"
-    }
+  buttonContainer: {
+    // flex: 1,
+    width: '100%',
+    borderBottomColor: '#521800',
+    borderBottomWidth: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'rgba(255,255,255, 0.3)',
+    padding: 10,
+  },
+  startButton: {
+    width: '100%',
+    padding: 12,
+    alignSelf: 'stretch',
+    backgroundColor: '#cc5500',
+    alignItems: 'center',
+  },
+  disableButton: {
+    width: '100%',
+    alignItems: 'center',
+    backgroundColor: '#rgba(0, 0, 0, 0.3)',
+    padding: 12,
+  },
+  startButtonText: {
+    color: 'white',
+  },
+  disableButtonText: {
+    color: '#787878',
+  },
 });
 
 export default startButton;
